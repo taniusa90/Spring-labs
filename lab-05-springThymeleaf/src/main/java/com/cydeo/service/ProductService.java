b0493a5d58bc00092555995a24051864181e9996
taniusa90 <galea_tanea@mail.com>
2022-10-06 09:13:43 -0500

    initial commit

---
+++ lab-05-springThymeleaf/src/main/java/com/cydeo/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductService {
+    boolean productCreate(Product product);
+    List<Product> listProduct();
+    Product findProductById(UUID uuid);
+}
